633986997033cf3623d9a975405d81e46c0139f2
Naty Martorelli <nmartorelli@gmail.com>
2022-10-21 11:29:15 -0300

    rellenando

---
+++ index.js
@@ -0,0 +1,25 @@
+const fs = require ('fs')
+
+class contenedor {
+    constructor (archivo) {
+        this.archivo
+
+    }
+
+
+}
+
+new constructor()
+
+/*Save(Object): Number - Recibe un objeto, lo guarda en el archivo, devuelve el id asignado.*/
+
+
+
+
+/*getById(Number): Object - Recibe un id y devuelve el objeto con ese id, o null si no está.*/
+
+/*getAll(): Object[] - Devuelve un array con los objetos presentes en el archivo.*/
+
+/*deleteById(Number): void - Elimina del archivo el objeto con el id buscado.*/
+
+/*deleteAll(): void - Elimina todos los objetos presentes en el archivo.*/
